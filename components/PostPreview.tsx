39958994884e7bfc6d8620d886d6b42aeb8718e1
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2024-03-20 01:43:49 +0900

    fix: no next support for building with decoded uri so renamed all posts, THANK YOU SO MUCH NEXT JS

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -5,7 +5,7 @@ import { ClockIcon } from "./Icons/ClockIcon";
 export default function PostPreview(props: PostMetadata) {
     return (
     <div className="rounded shadow-[0_2px_5px_-1px_rgba(0,0,0,0.16)] hover:shadow-[0_2px_5px_3px_rgba(0,0,0,0.16)] dark:shadow-[0_2px_5px_-1px_rgba(0,0,0,1.0)] dark:hover:shadow-[0_2px_5px_3px_rgba(0,0,0,1.0)] hover:text-gray-500 dark:hover:text-gray-400 hover:scale-x-105 transition duration-200 ease-in-out">
-      <Link href={`/posts/${props.subject}/${encodeURI(props.slug)}`}>
+      <Link href={`/posts/${props.subject}/${props.slug}`}>
         <div className="p-4">
           <div className="flex items-center justify-between">
             <span className="font-light text-sm">{ props.date }</span>

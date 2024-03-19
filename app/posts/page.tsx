155bedca52bbb2af836952151dcb11b623820a70
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2024-03-19 23:37:56 +0900

    fix: posts page fix

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,12 +1,16 @@
-"use client"
-
-import { useRouter } from "next/navigation";
+import getSubjectMetadata from "@/utils/getSubjectMetadata";
 
 export default function PostsPage(props: any) {
-    const router = useRouter()
-    router.push('/');
-
     return (
-        <>bye-bye</>
+        <div className="h-full flex flex-col justify-center items-center md:pl-10 z-0">
+            <div className="font-bold text-2xl text-gray-500 select-none">NOTHING FOUND</div>
+        </div>
     )
+}
+
+export const generateStaticParams = async () => {
+    const posts = getSubjectMetadata();
+    return posts!.map((post) => ({
+        subject: post.subject
+    }));
 }
